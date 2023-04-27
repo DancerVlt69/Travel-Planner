46b352c41b924c723520cb2a0f80b773093cf39d
DancerVlt69 <dancervlt69@googlemail.com>
2023-04-27 04:20:39 +0200

    some small changes

--- Travel-Planner/MainWindow.xaml.cs
+++ Travel-Planner/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Net;
 using System.Windows;
 using System.Xml;
@@ -20,23 +21,20 @@ namespace TravelPlanner
         // https://apis.deutschebahn.com/db-api-marketplace/apis/timetables/v1
         // Datenlizenz: Creative Common Attribution 4.0 International ( CC BY 4.0 )
         // by Deutsche Bahn AG
-        
-        private void search_IsInFocus(object sender, RoutedEventArgs e)
-        {
-            showStartTime.IsEnabled = false;
-        }
+
         private void search_Click(object sender, RoutedEventArgs eventArgs)
         {
             showStartTime.IsEnabled = false;
             var baseUrl = "https://apis.deutschebahn.com/db-api-marketplace/apis/timetables/v1/";
-            var urlEnc =  WebUtility.UrlEncode(inputTrainStation.Text);
+            var urlEnc = WebUtility.UrlEncode(inputTrainStation.Text);
+
             var client = new RestClient(baseUrl + "station/" + urlEnc);
             var request = new RestRequest("", Method.Get);
             request.AddHeader("DB-Client-Id", clientId);
             request.AddHeader("DB-Api-Key", clientAPI);
             request.AddHeader("accept", "application/xml");
             RestResponse response = client.Execute(request);
-           
+
             String s = response.Content.ToString();
             var doc = new XmlDocument();
             doc.LoadXml(s);
@@ -54,8 +52,52 @@ namespace TravelPlanner
             }
         }
 
-        private void showTime_Click(object sender, RoutedEventArgs eventArgs)
+         static class GetData
         {
+            
+             public static String getDBData()
+            {
+                var baseUrl = "https://apis.deutschebahn.com/db-api-marketplace/apis/timetables/v1/";
+                var startDate = DateTime.Now.ToString("yyMMdd");
+                var startTime = DateTime.Now.ToString("HH");
+                //var urlEnc = WebUtility.UrlEncode(inputTrainStation.Text);
+                //var stationId = ((CreateTrainStation)listTrainStations.SelectedItem).StationId;
+
+                return "";
+            }
+
+            private static String ClientUrlString (String baseUrl, String urlEncodeString)
+            {
+                var pathString = "station";
+                var clientUrlString = String.Format("{0}{1}/{2}", baseUrl, pathString, urlEncodeString);
+
+                return clientUrlString;
+            }
+
+            private static String ClientUrlString(String baseUrl, String stationId, String startDate, String startTime)
+            {
+                var pathString = "plan";
+                var clientUrlString = String.Format("{0}{1}/{2}/[3}/{4}", baseUrl, pathString, stationId, startDate, startTime);
+
+                return clientUrlString;
+            }
+
+            public static void requestData(RestClient client)
+            {
+                var request = new RestRequest("", Method.Get);
+                request.AddHeader("DB-Client-Id", clientId);
+                request.AddHeader("DB-Api-Key", clientAPI);
+                request.AddHeader("accept", "application/xml");
+                RestResponse response = client.Execute(request);
+                var doc = new XmlDocument();
+                
+                doc.LoadXml(response.Content.ToString());
+            }
+        }
+
+        private void showTime_Click(Object sender, RoutedEventArgs eventArgs)
+        {
+            listStartTime.Items.Clear();
             if (listTrainStations.SelectedItem == null) return;
 
             var baseUrl = "https://apis.deutschebahn.com/db-api-marketplace/apis/timetables/v1/";
@@ -64,6 +106,7 @@ namespace TravelPlanner
             var stationId = ((CreateTrainStation)listTrainStations.SelectedItem).StationId;
             
             var client = new RestClient(baseUrl + "plan/" + stationId + "/" + startDate + "/" + startTime);
+
             var request = new RestRequest("", Method.Get);
             request.AddHeader("DB-Client-Id", clientId);
             request.AddHeader("DB-Api-Key", clientAPI);
@@ -71,6 +114,8 @@ namespace TravelPlanner
             RestResponse response = client.Execute(request);
             var doc = new XmlDocument();
             doc.LoadXml(response.Content.ToString());
+
+
             var nodes = doc.GetElementsByTagName("s");
             var nodes2 = doc.GetElementsByTagName("dp");
 
@@ -82,16 +127,19 @@ namespace TravelPlanner
                 {
                     String travelTime = node2.Attributes["pt"].Value;
                     String destStation = node2.Attributes["ppth"].Value;
+                    String platformTrack = node2.Attributes["pp"].Value;
 
-                    NewTimeString newTimeString = new NewTimeString(travelTime);
+                    // TODO - String newViaStation = getNewViaString(destStation); 
+                    
                     NewDateString newDateString = new NewDateString(travelTime);
+                    NewTimeString newTimeString = new NewTimeString(travelTime);
+                    NewPlatform newPlatformString = new NewPlatform(platformTrack);
+                    String newDestStation = getNewStationString(destStation);
 
-                    String newListEntry = String.Format("{0} | {1} | {2}", newDateString, newTimeString, destStation);
+                    String newListEntry = String.Format(" {0}  |   {1}   |{2}|  {3}", newDateString, newTimeString, newPlatformString, newDestStation);
 
                     listStartTime.Items.Add(newListEntry);
 
-                    // string[] newStationArray = stationName.Split('|');
-                    // int count = newStationArray.Length;
                 }
             }    
         }
@@ -99,6 +147,46 @@ namespace TravelPlanner
         private void listTrainStations_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
             showStartTime.IsEnabled = true;
+        }
+
+        public static String getNewStationString(String stationString)
+        {
+            string[] stationStringArray = stationString.Split('|');
+
+            var newViaString = "";
+
+            if (!(stationStringArray.Length <= 1))
+            {
+                for (int i = 0; i < stationStringArray.Length - 1; i++)
+                {
+                    if (i < stationStringArray.Length - 2)
+                    {
+                        newViaString += stationStringArray[i] + ", ";
+                    }
+                    else
+                    {
+                        newViaString += stationStringArray[i];
+                    }
+                }
+            }
+
+            var newDestString = stationStringArray[stationStringArray.Length - 1];
+
+            if (newViaString.Length != 0)
+            {
+                var newStationString = String.Format("{0} via {1}", newDestString, newViaString);
+                return newStationString;
+            }
+
+            else
+            {
+                var newStationString = String.Format("{0}", newDestString);
+                return newStationString;
+            }
+
+            //Console.WriteLine(); // only for Debug
+            // Console.WriteLine(newStationString); // only for Debug
+            
         }
 
         class CreateTrainStation
@@ -201,8 +289,42 @@ namespace TravelPlanner
             }
         }
 
+        class NewPlatform
+        {
+            public NewPlatform(String platformTrack)
+            {
+                this.platformTrack = platformTrack;
+            }
 
+            String platformTrack; String newPlatformTrack;
 
+            public String PlaformTrack { get { return platformTrack; } }
+
+            public String NewPlatformTrack
+            {
+                get
+                {
+                    String nPT = String.Format("Track {0}", platformTrack);
+                    int c1 = platformTrack.Length;
+                    int c2 = nPT.Length;
+                    int c3;
+                    if (c1 >= 2) { c3 = 14; } else { c3 = 15; }
+                    while (c2 < c3)
+                    {
+                        nPT = nPT.PadLeft(c2 + 1, ' ').PadRight(c2 + 2, ' ');
+                     
+                        c2 = nPT.Length;
+                    }
+                    
+                    return nPT;
+                }
+            }
+            public override String ToString()
+            {
+                return NewPlatformTrack;
+            }
+        }
+       
 
 
 
